95b38a279a21699c432653b3f6b3347fa297735e
hassnaelhaili <82993786+hassnaelhaili@users.noreply.github.com>
2022-12-27 00:37:17 +0100

    complete layouts and landing page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ mix('/resources/css/landing.css')}}">
+    <title>Midnight</title>
+</head>
+<body>
+<div class="navBar">
+    <div class="navLeft">
+        <div class="logo">Midnight</div>
+        <div class="navLinks">
+            <ul>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">link</a></li>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">link</a></li>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">link</a></li>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">link</a></li>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">link</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="navRight">
+        <div class="nav-search">
+            <input type="search" name="search" id="test"><i id="ic" class="fa-sharp fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div class="sign">
+            <a href="/login" target="_blank" rel="noopener noreferrer"><button type="submit">Sign in <i class="fa-solid fa-right-to-bracket"></i></button></a>
+            <h5>or</h5>
+            <a href="/register" target="_blank" rel="noopener noreferrer"><button type="submit">Sign up&nbsp;<i class="fa-solid fa-user-plus"></i></button></a>
+        </div>
+    </div>
+</div>
+
+<div class="welcomePageView">
+    <div class="vid">
+        <div class="para">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, corporis non, consectetur nisi mollitia velit ratione saepe sunt labore reiciendis assumenda. Quas cum eum incidunt corrupti optio dolores consectetur! In?</p>
+        </div>
+        <video autoplay muted loop id="myVideo" width="100%" >
+            <source src="/video/midnightt.mp4" type="video/mp4">
+        </video>
+    </div>
+</div>
+
+<div class="about">
+    <div class="container">
+        <div class="container-left">
+            <div class="para">
+                <h4>Who are we ?!</h4>
+                <h1>Midnight</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda sequi culpa recusandae aperiam. In illum, nihil debitis iusto reiciendis perspiciatis ducimus quae, odit ipsa non quo molestias perferendis similique praesentium!</p>
+            </div>
+        </div>
+        <div class="container-box">
+            <h3>picture here of the team</h3>
+        </div>
+        <div class="container-right"></div>
+    </div>
+</div>
+
+
+
+<footer>
+    <div class="head">
+        <h1>Midnight</h1>
+    </div>
+    <div class="footerBody">
+        <div class="list">
+            <h2>about</h2>
+            <ul>
+                <li>servise</li>
+                <li>servise</li>
+                <li>servise</li>
+                <li>servise</li>
+            </ul>
+        </div>
+        <div class="list">
+            <h2>policy</h2>
+            <ul>
+                <li>servise</li>
+                <li>servise</li>
+                <li>servise</li>
+                <li>servise</li>
+            </ul>
+        </div>
+        <div class="list">
+            <h2>company</h2>
+            <ul>
+                <li>servise</li>
+                <li>servise</li>
+                <li>servise</li>
+                <li>servise</li>
+            </ul>
+        </div>
+        <div class="list">
+            <h2>clients</h2>
+            <ul>
+                <li>servise</li>
+                <li>servise</li>
+                <li>servise</li>
+                <li>servise</li>
+            </ul>
+        </div>
+    </div>
+    <div class="contact">
+        <div class="sm">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-square-instagram"></i>
+            <i class="fa-brands fa-square-twitter"></i>
+            <i class="fa-solid fa-envelopes-bulk"></i>
+            <i class="fa-brands fa-linkedin"></i>
+        </div>
+        <div class="subs">
+            <label for="email">subscribe</label>
+            <input type="email" name="" id="" placeholder="GET IN TOUCH">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cumque accusantium alias, eos dignissimos debitis iusto ullam ea soluta, eius recusandae? Ratione, qui quod. </p>
+        </div>
+    </div>
+    <div class="copyright">
+        <p>2022 <i class="fa fa-copyright" aria-hidden="true"></i> midnight-All rights reserved-Terms of Use-Privacy Policy </p>
+    </div>
+</footer>
+<script src="https://kit.fontawesome.com/ce35cf2649.js" crossorigin="anonymous"></script>
+<script src="/landingPage/index.js"></script>
+</body>
+</html>
