a0e4e6416c0f2bfe56f155a06ce2930ae860f078
hassnaelhaili <82993786+hassnaelhaili@users.noreply.github.com>
2022-12-27 00:38:55 +0100

    Create profile.blade.php

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ mix('/resources/css/profile.css')}}">
+
+    <title>Midight</title>
+</head>
+<body>
+
+<div class="navBar">
+    <div class="responsive">
+        <div class="menu"><a href=""  rel="noopener noreferrer"><i class="fa-solid fa-bars"></i></a></div>
+        <div class="logo1"><a href=""rel="noopener noreferrer">MD</a></div>
+        <div class="search-Ic"><a href="http://" rel="noopener noreferrer"><i class="fa-solid fa-magnifying-glass"></i></a></div>
+    </div>
+    <div class="container-logo">
+        <div class="logo"><strong> Midnight</strong></div>
+
+        <div class="navLinks">
+            <ul class="Links">
+                <li><a href=""  rel="noopener noreferrer">Find agencies<i id="fle" class="fa-solid fa-caret-down"></i></a>
+
+                </li>
+                <li><a id="this" href=""  rel="noopener noreferrer">Resources<i id="fle" class="fa-solid fa-caret-down"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="container-search">
+        <dir class="search">
+            <input id="locaction-field" type="search" placeholder="Where?">
+            <input id="service-field" type="text"  class="search" placeholder="What service?" >
+        </dir>
+        <div class="logInOut">
+            <button class="btn">Post a project</button><br>
+            <i id="Profile-icon" class="fa-solid fa-user"></i>
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="container-profile">
+    <img class="profile-Img" src="/Capture.PNG" alt="" srcset="">
+
+    <div class="profile-nav">
+        <img src="/midnight.png" alt="" srcset="">
+        <ul class="profile-nav-links">
+            <li class="for-border"><a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-address-card"></i>About</a></li>
+            <li class="for-border"><a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-pen-to-square"></i>Services</a></li>
+            <li class="for-border"><a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-circle-info"></i>Portfolio</a></li>
+            <li class="for-border"><a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-people-group"></i>Team</a></li>
+            <li class="for-border"><a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-award"></i>Awards</a></li>
+            <li class="for-border"><a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-star"></i>Reviews</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-envelope"></i>Contact</a></li>
+        </ul>
+    </div>
+
+    <div class="about-section">
+        <div class="about-description-container">
+            <div class="profile-name"><h1>Profile Name</h1></div>
+            <div class="profile-location"><h6>City, Country</h6></div>
+        <a href="/AgenciesListing/about">test</a>
+            <br>
+            <div class="infos">
+                <dir class="left">
+                    <ul>
+                        <li>infos</li>
+                        <li>infos</li>
+                        <li>infos</li>
+                        <li>infos</li>
+                    </ul>
+                </dir>
+                <dir class="right">
+                    <ul>
+                        <li>infos</li>
+                        <li>infos</li>
+                        <li>infos</li>
+                        <li>infos</li>
+                    </ul>
+                </dir>
+            </div>
+        </div>
+        <div class="about-rating-container">
+            <div class="rating-box">
+                <input type="radio" name="star" id="star1"><label for="star1"></label>
+                <input type="radio" name="star" id="star2"><label for="star2"></label>
+                <input type="radio" name="star" id="star3"><label for="star3"></label>
+                <input type="radio" name="star" id="star4"><label for="star4"></label>
+                <input type="radio" name="star" id="star5"><label for="star5"></label>
+            </div>
+            <div class="contact-button">
+                <button id="contact-btn">contact</button>
+                <button id="site-btn">go to website</button>
+            </div>
+        </div>
+    </div>
+    <hr id="lign-one">
+    <div class="titleServices">
+        <h1 class="title">Services</h1>
+        <p id="subTitle">Services offered by Markable</p>
+    </div>
+    <div class="header">
+        <ul>
+            <li id="Sname">Service name</li>
+            <li>Experience level</li>
+            <li>related reviews</li>
+            <li>starting from</li>
+        </ul>
+    </div>
+    <div class="listOfServices">
+        
+        <div class="event">
+            <ul>
+                <li><i class="fa-solid fa-calendar"></i>eventttt</li>
+                <li>******workd</li>
+                <li>rating</li>
+                <li>price/poject</li>
+                <li><i id="fle" class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div><hr class="devider">
+        <div class="event">
+            <ul>
+                <li><i class="fa-solid fa-calendar"></i>event</li>
+                <li>******workd</li>
+                <li>rating</li>
+                <li>price/poject</li>
+                <li><i id="fle" class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div><hr class="devider">
+        <div class="event">
+            <ul>
+                <li><i class="fa-solid fa-calendar"></i>event</li>
+                <li>******workd</li>
+                <li>rating</li>
+                <li>price/poject</li>
+                <li><i id="fle" class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div><hr class="devider">
+        <div class="event">
+            <ul>
+                <li><i class="fa-solid fa-calendar"></i>event</li>
+                <li>******workd</li>
+                <li>rating</li>
+                <li>price/poject</li>
+                <li><i id="fle" class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div><hr class="devider">
+        <div class="event">
+            <ul>
+                <li><i class="fa-solid fa-calendar"></i>event</li>
+                <li>******workd</li>
+                <li>rating</li>
+                <li>price/poject</li>
+                <li><i id="fle" class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div><hr class="devider">
+        <div class="event">
+            <ul>
+                <li><i class="fa-solid fa-calendar"></i>event</li>
+                <li>******workd</li>
+                <li>rating</li>
+                <li>price/poject</li>
+                <li><i id="fle" class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div><hr class="devider">
+        <div class="event">
+            <ul>
+                <li><i class="fa-solid fa-calendar"></i>event</li>
+                <li>******workd</li>
+                <li>rating</li>
+                <li>price/poject</li>
+                <li><i id="fle" class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div><hr class="devider">
+        <div class="event">
+            <ul>
+                <li><i class="fa-solid fa-calendar"></i>event</li>
+                <li>******workd</li>
+                <li>rating</li>
+                <li>price/poject</li>
+                <li><i id="fle" class="fa-solid fa-caret-down"></i></li>
+            </ul>
+        </div>
+    </div>
+    <hr id="lign-one">
+    <div class="titleServices">
+        <h1 class="title">Portfolio</h1>
+        <p id="subTitle">Services offered by Markable</p>
+    </div>
+    <!--I CAN REPLACE THIS SECTION WITH THE grid-->
+    <div class="portfolio">
+        <div class="box-potfolio">
+            <img class="images" src="midnight.png" alt="" srcset="">
+            <p class="par">Lorem ipsum, dolor sit amet consectetur c<a href="http://" target="_blank" rel="noopener noreferrer">adipisicing</a> .</p>
+        </div>
+        <div class="box-potfolio">
+            <img class="images" src="midnight.png" alt="" srcset="">
+            <p class="par">Lorem ipsum, dolor sit amet consectetur c<a href="http://" target="_blank" rel="noopener noreferrer">adipisicing</a> .</p>
+        </div>
+        <div class="box-potfolio">
+            <img class="images" src="midnight.png" alt="" srcset="">
+            <p class="par">Lorem ipsum, dolor sit amet consectetur c<a href="http://" target="_blank" rel="noopener noreferrer">adipisicing</a> .</p>
+        </div>
+    </div>
+    <div class="portfolio1">
+        <div class="box-potfolio">
+            <img class="images" src="midnight.png" alt="" srcset="">
+            <p class="par">Lorem ipsum, dolor sit amet consectetur c<a href="http://" target="_blank" rel="noopener noreferrer">adipisicing</a> .</p>
+        </div>
+        <div class="box-potfolio">
+            <img class="images" src="midnight.png" alt="" srcset="">
+            <p class="par">Lorem ipsum, dolor sit amet consectetur c<a href="http://" target="_blank" rel="noopener noreferrer">adipisicing</a> .</p>
+        </div>
+        <div class="box-potfolio">
+            <img class="images" src="midnight.png" alt="" srcset="">
+            <p class="par">Lorem ipsum, dolor sit amet consectetur c<a href="http://" target="_blank" rel="noopener noreferrer">adipisicing</a> .</p>
+        </div>
+    </div>
+    <hr id="lign-one">
+    <div class="titleServices">
+        <h1 class="title">Team</h1>
+        <p id="subTitle">Team members in Markable</p>
+    </div>
+    <div class="team-pic">
+        <img src="/Capture.PNG" alt="" srcset="">
+    </div>
+
+
+    </div>
+</div>
+@include('layouts.footer')
+
+
+
+
+
+
+
+
+<script src="https://kit.fontawesome.com/ce35cf2649.js" crossorigin="anonymous"></script>
+<script src="landingP.js"></script>
+</body>
+</html>
